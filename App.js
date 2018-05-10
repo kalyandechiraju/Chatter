358c56aac104eb96d2327462a9ad2bf1e559ef1c
Kalyan Dechiraju <kalyandechiraju@outlook.com>
2018-05-10 12:17:36 +0530

    :zap: Add eslint rules and firebase config

--- App.js
+++ App.js
@@ -1,42 +1,14 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React, { Component } from 'react'
+import { Platform, StyleSheet, Text, View } from 'react-native'
+import firebase from 'firebase'
+import { config } from './firebaseConfig'
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+firebase.initializeApp(config)
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
-}
+  ios: 'Press Cmd+R to reload,\n Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n Shake or press menu button for dev menu',
+})
 
 const styles = StyleSheet.create({
   container: {
@@ -55,4 +27,19 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
-});
+})
+
+// type Props = {};
+export default class App extends Component {
+  componentDidMount() {}
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
+    )
+  }
+}
